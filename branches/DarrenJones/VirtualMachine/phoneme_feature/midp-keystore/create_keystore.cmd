c7bb5c971450816c2187ef8d00a489196909fe23
jonestheguitar <jonestheguitar@d08643f4-6afd-11de-bb38-9f61fe354615>
2009-07-16 15:01:21 +0000

    merge r15:16 Catchup vm from trunk

--- branches/DarrenJones/VirtualMachine/phoneme_feature/midp-keystore/create_keystore.cmd
+++ branches/DarrenJones/VirtualMachine/phoneme_feature/midp-keystore/create_keystore.cmd
@@ -4,3 +4,4 @@ echo Creating device key store
 keytool -import -alias rootCA -file ..\certificates\BlueWhaleSystems_CA\cacert.cer -keystore main.ks -storepass 3edcvgy76tfcxsw2
 keytool -import -alias manufacturerCA -file ..\certificates\BlueWhaleSystems_CA\certs\intermediatecert.cer -keystore main.ks -storepass 3edcvgy76tfcxsw2
 keytool -list -v -keystore main.ks -storepass 3edcvgy76tfcxsw2
+
