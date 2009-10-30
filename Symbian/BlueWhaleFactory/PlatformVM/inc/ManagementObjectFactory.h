82c84dbac8643c74ad2be7bc4fa945297685b001
jonestheguitar <jonestheguitar@d08643f4-6afd-11de-bb38-9f61fe354615>
2009-10-30 14:26:04 +0000

    workitem #3628 Symbian: Expose phoneme_feature's app manager UI and re-enable it as the first screen shown when BlueWhale is started without a shortcut Launch appropriate MIDlet based on shortcut, autostart and MIDlets currently installed

--- Symbian/BlueWhaleFactory/PlatformVM/inc/ManagementObjectFactory.h
+++ Symbian/BlueWhaleFactory/PlatformVM/inc/ManagementObjectFactory.h
@@ -41,6 +41,8 @@ const TInt KIID_MVMObjectFactory = 0xA0003F58;
 const TInt KIID_MVMObjectFactoryClient = 0xA0003F50;
 const TInt KIID_MShortcutName = 0xA0003F52;
 const TString8UniqueKey KPropertyString8ShortcutName		= { KIID_MShortcutName, 10101 };
+const TInt KIID_MAutoStarted = 0xA0003F53;
+const TIntUniqueKey KPropertyIntAutoStarted					= { KIID_MAutoStarted, 10101 };
 
 class MVMObjectFactory : public MUnknown
 {
