95c53efce39c26dda6c30ad9b9de13f72d3b6856
jonestheguitar <jonestheguitar@d08643f4-6afd-11de-bb38-9f61fe354615>
2010-02-08 12:02:58 +0000

    bug #3929 Nokia Series40 support Adjusted splashscreen to match midlet

--- VirtualMachine/phoneme_feature/midp/src/ams/example/jams/native/SplashScreenCanvas.c
+++ VirtualMachine/phoneme_feature/midp/src/ams/example/jams/native/SplashScreenCanvas.c
@@ -30,64 +30,17 @@
 
 #include <gxj_putpixel.h>
 
-int AdjustColor(int aStartColor, int aEndColor, int aTotal, int aCurrent)
-{
-	int delta;
-	float proportion;
-	if( aStartColor == aEndColor )
-	{
-		return aStartColor;
-	}
-	proportion = ( (float) aCurrent ) / ( (float) aTotal );
-	delta = (int) ( (aEndColor - aStartColor ) * proportion );
-	return aStartColor + delta;
-}
-
 void DrawSplashScreen()
 {
-	int startColor = 0x44BEF6;
-	int endColor   = 0x142A64;
+	int fillColor   = 0x142A64;
 	jshort clip[4];
-	int height;
-	int currentHeight;
-
-	int startRed = startColor >> 16 & 0xff;
-	int startGreen = startColor >> 8 & 0xff;
-	int startBlue = startColor & 0xff;
-	int endRed = endColor >> 16 & 0xff;
-	int endGreen = endColor >> 8 & 0xff;
-	int endBlue = endColor & 0xff;
-	int width;
-	if (gxj_system_screen_buffer.width > gxj_system_screen_buffer.height)
-	{
-		width = 2 * gxj_system_screen_buffer.width;
-	}
-	else
-	{
-		width = 2 * gxj_system_screen_buffer.height;
-	}
-	height = width;
-	currentHeight = height;
 	
 	clip[0] = 0;
 	clip[1] = 0;
 	clip[2] = gxj_system_screen_buffer.width;
 	clip[3] = gxj_system_screen_buffer.height;
 
-	draw_roundrect(GXJ_RGB24TORGB16(endColor), clip, &gxj_system_screen_buffer, 0, 0, 0, gxj_system_screen_buffer.width, gxj_system_screen_buffer.height, 1, 0, 0);
-
-	while( ( 0 < width ) && ( 0 < height ) )
-	{
-		int r = AdjustColor( startRed, endRed, height, currentHeight );
-		int g = AdjustColor( startGreen, endGreen, height, currentHeight );
-		int b = AdjustColor( startBlue, endBlue, height, currentHeight );
-		int color = ( ( r << 16 ) & 0xff0000 ) | ( ( g << 8 ) & 0xff00 ) | ( b & 0xff );
-
-		draw_arc(GXJ_RGB24TORGB16(color), clip, &gxj_system_screen_buffer, 0, gxj_system_screen_buffer.width - (width / 2), (currentHeight / 2) - currentHeight, width, currentHeight, 1, 0, 360);
-
-		width -= 2;
-		currentHeight -= 2;
-	}
+	draw_roundrect(GXJ_RGB24TORGB16(fillColor), clip, &gxj_system_screen_buffer, 0, 0, 0, gxj_system_screen_buffer.width, gxj_system_screen_buffer.height, 1, 0, 0);
 }
 
 KNIEXPORT KNI_RETURNTYPE_VOID Java_com_bluewhalesystems_midp_SplashScreenCanvas_DrawSplashScreen0()
